58b62ba5285e479db2070df0aa12e11d0a772542
Nate Tan <nktan93@gmail.com>
2019-07-23 18:12:43 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,12 @@
 class Song < ApplicationRecord
+  validates :title, presence: true
 
+  def title_cannot_be_repeated_by_same_artist_in_same_year
+    def true?
+      songs_with_same_title = Song.all.select do |song|
+        song.title == title
+      end
+      !!song_with_same_title.year 
+    end
+  end
 end
