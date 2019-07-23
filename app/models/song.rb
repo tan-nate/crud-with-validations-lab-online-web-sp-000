59b4c25dc0eb1d96a0d2c3713fc371a9280c359e
Nate Tan <nktan93@gmail.com>
2019-07-23 18:26:58 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,11 +2,15 @@ class Song < ApplicationRecord
   validates :title, presence: true
 
   def title_cannot_be_repeated_by_same_artist_in_same_year
-    def true?
-      songs_with_same_title = Song.all.select do |song|
-        song.title == title
+    def fails_validation?
+      songs_with_same_title_and_year = Song.all.select do |song|
+        song.title == title && song.year == year
       end
-      !!song_with_same_title.year
+
+      !songs_with_same_title_and_year.empty?
     end
+
+    if fails_validation?
+      errors.add(:title, "can't be repeated by the same artist in the same year")
   end
 end
