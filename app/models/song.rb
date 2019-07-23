43e240c197e8c6c026ac55ea34465facbc9f2b35
Nate Tan <nktan93@gmail.com>
2019-07-23 18:45:38 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ApplicationRecord
   validates :title, presence: true
   validates :released, inclusion: {in: %w(true false)}
-  validates :release_year, 
+  validates :release_year,
   validate :title_cannot_be_repeated_by_same_artist_in_same_year
 
   def title_cannot_be_repeated_by_same_artist_in_same_year
@@ -21,7 +21,7 @@ class Song < ApplicationRecord
   def if_released_must_have_release_year
     def passes_validation?
       if released && release_year <= Time.new.year
-        
+
     end
   end
 end
