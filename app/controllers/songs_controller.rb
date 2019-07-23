af2d5f256c4a35910214affed48a040508b1b189
Nate Tan <nktan93@gmail.com>
2019-07-23 17:24:25 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,9 +1,21 @@
 class SongsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @songs = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
+  end
+
+  def edit
+    
+  end
+
+  private
+
+  def set_post
     @song = Song.find(params[:id])
   end
 end
